571776fecfca0644dd1d53a02492b60293321bc0
Ratnadeep Bhattacharyya <ratndeepofghato@gmail.com>
2023-02-14 14:35:10 +0530

    fix test mongodb memory not exiting properly

--- src/mock/mongoMock.ts
+++ src/mock/mongoMock.ts
@@ -1,5 +1,6 @@
 import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import { disconnect } from 'mongoose';
 
 let mongod: MongoMemoryServer;
 
@@ -16,5 +17,6 @@ export const rootMongooseTestModule = (options: MongooseModuleOptions = {}) =>
   });
 
 export const closeInMongodConnection = async () => {
+  await disconnect();
   if (mongod) await mongod.stop();
 };
